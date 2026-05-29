12084ef610e94e742e1903ff0807f25015aff454
mennatullahalii <mennatullahalii.dev@gmail.com>
2026-05-29 13:36:34 +0200

    github io

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Forces Next.js to build a static HTML/CSS/JS site
+  output: "export",
+
+  // Disables the Next.js image optimization server
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
